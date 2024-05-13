a17666f8ac7aef57a52fd73e4e681243ae94f730
Otshepahetse Tinte <tintelehlohonolo359@gmail.com>
2024-05-13 18:42:18 +0200

    Added Iterater

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,5 +14,34 @@ public class Main {
         names.add("Rorisang");
 
         System.out.println("The Names of the Big Five are :"+ names);
+
+        //adding more names
+        names.addFirst("Trust");
+        names.addLast("Primo");
+        names.add(3,"Tinte");
+
+        System.out.println("The updated names are :" + names);
+
+        //deleting names
+        names.remove(3);
+        System.out.println("You now have 7 names :" + names);
+
+        //Accessing the Elements
+        String firstName = names.getFirst();
+        String lastName = names.getLast();
+        String nameAtIndex = names.get(3);
+
+        System.out.println("First name : " + firstName);
+        System.out.println("Last name : " + lastName);
+        System.out.println("Name at Index : " + nameAtIndex);
+
+        //Iterating names
+        ListIterator<String> iterator = names.listIterator();
+
+        while(iterator.hasNext()){
+
+            System.out.println(iterator.next());
+        }
+        System.out.println("The iterator wordes");
     }
 }

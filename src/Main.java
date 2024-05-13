6474fa5e0d84925e5623b58b5dca18d0b2ac4490
Otshepahetse Tinte <tintelehlohonolo359@gmail.com>
2024-05-13 18:10:58 +0200

    First Change

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+
+        LinkedList <String> names = new  LinkedList<>();
+
+        names.add("Lehlohonolo");
+        names.add("Keamogetswe");
+        names.add("Tshireletso");
+        names.add("Luyanda");
+        names.add("Rorisang");
+
+        System.out.println("The Names of the Big Five are :"+ names);
     }
 }
